3925cc0f65827b7daa0486074d4d9f575134c5b0
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-01-25 06:55:59 +0000

    remove functions now always included by utility.functions.php

--- install.php
+++ install.php
@@ -12,18 +12,6 @@
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 
-if (! function_exists("out")) {
-	function out($text) {
-		echo $text."<br />";
-	}
-}
-
-if (! function_exists("outn")) {
-	function outn($text) {
-		echo $text;
-	}
-}
-
 global $db;
 global $amp_conf;
 
