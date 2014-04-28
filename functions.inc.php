d10f3ebf48d1f8769d2cf1e4d7bffcfa88fc384d
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-28 15:12:12 -0500

    [Module Tag script: outroutemsg 2.11.0.2]

--- functions.inc.php
+++ functions.inc.php
@@ -2,7 +2,6 @@
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2013 Schmooze Com Inc.
-//  Copyright (C) 2009 Philippe Lindheimer 
 //  Copyright (C) 2010 Mikael Carlsson
 //
 
