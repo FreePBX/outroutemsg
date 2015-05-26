465e353158ceb6fb054d997a2f7b34000bb1b829
Andrew Nagy <andrew.nagy@the159.com>
2015-05-26 11:17:31 -0400

    [Module Tag script: outroutemsg 13.0.1alpha1]

---
+++ views/main.php
@@ -0,0 +1,22 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-9">
+			<div class="fpbx-container">
+				<form class="fpbx-submit" name="frm_extensions" action="config.php?display=extensions" method="post" data-fpbx-delete="config.php?display=extensions&amp;extdisplay=1000&amp;action=del" role="form">
+					<ul class="nav nav-tabs" role="tablist">
+						<li data-name="tab1" class="change-tab active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Tab1 (active!)</a></li>
+						<li data-name="tab2" class="change-tab"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab">Tab2</a></li>
+					</ul>
+					<div class="tab-content display">
+						<div id="tab1" class="tab-pane active">
+							Tab 1 Data (Shown)
+						</div>
+						<div id="tab2" class="tab-pane">
+							Tab 2 Data (Hidden)
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
