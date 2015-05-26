1a3e9e74a25120ee186b502deb43e15b2ffb76ca
James Finstrom <jfinstrom@sangoma.com>
2015-05-26 14:24:17 -0400

    13 visual updates

--- page.outroutemsg.php
+++ page.outroutemsg.php
@@ -16,7 +16,7 @@ if($action){
 	$default_msg_id      = isset($_REQUEST['default_msg_id'])      ? trim($_REQUEST['default_msg_id'])      : DEFAULT_MSG;
 	$intracompany_msg_id = isset($_REQUEST['intracompany_msg_id']) ? trim($_REQUEST['intracompany_msg_id']) : DEFAULT_MSG;
 	$emergency_msg_id    = isset($_REQUEST['emergency_msg_id'])    ? trim($_REQUEST['emergency_msg_id'])    : DEFAULT_MSG;
-	$no_answer_msg_id    = isset($_REQUEST['no_answer_msg_id'])    ? trim($_REQUEST['no_answer_msg_id'])    : DEFAULT_MSG;	
+	$no_answer_msg_id    = isset($_REQUEST['no_answer_msg_id'])    ? trim($_REQUEST['no_answer_msg_id'])    : DEFAULT_MSG;
 	$invalidnmbr_msg_id  = isset($_REQUEST['invalidnmbr_msg_id'])  ? trim($_REQUEST['invalidnmbr_msg_id'])  : DEFAULT_MSG;
 
 	if ($action == 'submit') {
@@ -38,106 +38,206 @@ if ($action != 'submit') {
 }
 
 ?>
-<h2><?php echo _("Route Congestion Messages")?></h2>
-<h4><?php echo _("No Routes Available")?></h4>
-<form name="outroutemsg" action="config.php" method="post">
-<input type="hidden" name="display" value="<?php echo $dispnum ?>"/>
-<input type="hidden" name="action" value="submit"/>
-<table>
-<tr><td colspan="2"><h5><?php echo _("Standard Routes")?><hr></h5></td></tr>
-<tr>
-	<td><a href="#" class="info"><?php echo _("Message or Tone")?><span><?php echo _("Message or tone to be played if no trunks are available.")?></span></a></td>
-	<td align=right>
-		<select name="default_msg_id" id="default_msg_id" tabindex="<?php echo ++$tabindex;?>">
-		<?php
-			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $default_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
-			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $default_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
-			if (isset($tresults[0])) {
-				foreach ($tresults as $tresult) {
-					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $default_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
-				}
-			}
-		?>
-		</select>
-	</td>
-</tr>
-
-<tr><td colspan="2"><h5><?php echo _("Intra-Company Routes")?><hr></h5></td></tr>
-<tr>
-	<td><a href="#" class="info"><?php echo _("Message or Tone")?><span><?php echo _("Message or tone to be played if no trunks are available. Used on routes marked as intra-company only.")?></span></a></td>
-	<td align=right>
-		<select name="intracompany_msg_id" id="intracompany_msg_id" tabindex="<?php echo ++$tabindex;?>">
-		<?php
-			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
-			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
-			if (isset($tresults[0])) {
-				foreach ($tresults as $tresult) {
-					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $intracompany_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
-				}
-			}
-		?>
-		</select>
-	</td>
-</tr>
-
-<tr><td colspan="2"><h5><?php echo _("Emergency Routes")?><hr></h5></td></tr>
-<tr>
-	<td><a href="#" class="info"><?php echo _("Message or Tone")?><span><?php echo _("Message or tone to be played if no trunks are available. Used on all emergency routes. Consider a message instructing callers to find an alternative means of calling emergency services such as a cell phone or alarm system panel.")?></span></a></td>
-	<td align=right>
-		<select name="emergency_msg_id" id="emergency_msg_id" tabindex="<?php echo ++$tabindex;?>">
-		<?php
-			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $emergency_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
-			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $emergency_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
-			if (isset($tresults[0])) {
-				foreach ($tresults as $tresult) {
-					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $emergency_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
-				}
-			}
-		?>
-		</select>
-	</td>
-</tr>
-<tr><td colspan="2"><br><h4><?php echo _("Trunk Failures")?></h4></td></tr>
-
-<tr><td colspan="2"><h5><?php echo _("No Answer")?><hr></h5></td></tr>
-<tr>
-	<td><a href="#" class="info"><?php echo _("Message or Tone")?><span><?php echo _("Message or tone to be played if there was no answer. Default message is:<br>\"The number is not answering.\"<br> Hangupcause is 18 or 19")?></span></a></td>
-	<td align=right>
-		<select name="no_answer_msg_id" id="no_answer_msg_id" tabindex="<?php echo ++$tabindex;?>">
-		<?php
-			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $no_answer_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
-			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $no_answer_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
-			if (isset($tresults[0])) {
-				foreach ($tresults as $tresult) {
-					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $no_answer_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
-				}
-			}
-		?>
-		</select>
-	</td>
-</tr>
-
-<tr><td colspan="2"><h5><?php echo _("Number or Address Incomplete")?><hr></h5></td></tr>
-<tr>
-	<td><a href="#" class="info"><?php echo _("Message or Tone")?><span><?php echo _("Message or tone to be played if trunk reports Number or Address Incomplete. Usually this means that the number you have dialed is to short. Default message is:<br>\"The number you have dialed is not in service. Please check the number and try again.\"<br>Hangupcause is 28")?></span></a></td>
-	<td align=right>
-		<select name="invalidnmbr_msg_id" id="invalidnmbr_msg_id" tabindex="<?php echo ++$tabindex;?>">
-		<?php
-			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $invalidnmbr_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
-			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $invalidnmbr_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
-			if (isset($tresults[0])) {
-				foreach ($tresults as $tresult) {
-					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $invalidnmbr_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
-				}
-			}
-		?>
-		</select>
-	</td>
-</tr>
-
-<tr>
-	<td colspan="2"><br><h6><input name="Submit" type="submit" value="<?php echo _("Submit Changes")?>" tabindex="<?php echo ++$tabindex;?>"></h6></td>
-</tr>
-</table>
-
-</form>
+<div class="container-fluid">
+	<h1><?php echo _('Route Congestion Messages')?></h1>
+	<div class = "display full-border">
+		<div class="row">
+			<div class="col-sm-9">
+				<div class="fpbx-container">
+					<form name="outroutemsg" id="outroutemsg" class='fpbx-submit' action="config.php" method="post">
+					<input type="hidden" name="display" value="outroutemsg"/>
+					<input type="hidden" name="action" value="submit"/>
+						<ul class="nav nav-tabs" role="tablist">
+							<li role="presentation" data-name="noroute" class="active">
+								<a href="#noroute" aria-controls="noroute" role="tab" data-toggle="tab">
+									<?php echo _("No Routes Available")?>
+								</a>
+							</li>
+							<li role="presentation" data-name="notrunk" class="change-tab">
+								<a href="#notrunk" aria-controls="notrunk" role="tab" data-toggle="tab">
+									<?php echo _("Trunk Failures")?>
+								</a>
+							</li>
+						</ul>
+						<div class="tab-content display">
+							<div role="tabpanel" id="noroute" class="tab-pane active">
+								<!--Standard Routes-->
+								<div class="element-container">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="row">
+												<div class="form-group">
+													<div class="col-md-3">
+														<label class="control-label" for="default_msg_id"><?php echo _("Standard Routes") ?></label>
+														<i class="fa fa-question-circle fpbx-help-icon" data-for="default_msg_id"></i>
+													</div>
+													<div class="col-md-9">
+														<select class="form-control" id="default_msg_id" name="default_msg_id">
+															<?php
+																echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $default_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
+																echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $default_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+																if (isset($tresults[0])) {
+																	foreach ($tresults as $tresult) {
+																		echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $default_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
+																	}
+																}
+															?>
+														</select>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<span id="default_msg_id-help" class="help-block fpbx-help-block"><?php echo _("Message or tone to be played if no trunks are available.")?></span>
+										</div>
+									</div>
+								</div>
+								<!--END Standard Routes-->
+								<!--Intra-Company Routes-->
+								<div class="element-container">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="row">
+												<div class="form-group">
+													<div class="col-md-3">
+														<label class="control-label" for="intracompany_msg_id"><?php echo _("Intra-Company Routes") ?></label>
+														<i class="fa fa-question-circle fpbx-help-icon" data-for="intracompany_msg_id"></i>
+													</div>
+													<div class="col-md-9">
+														<select class="form-control" id="intracompany_msg_id" name="intracompany_msg_id">
+															<?php
+																echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
+																echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+																if (isset($tresults[0])) {
+																	foreach ($tresults as $tresult) {
+																		echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $intracompany_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
+																	}
+																}
+															?>
+														</select>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<span id="intracompany_msg_id-help" class="help-block fpbx-help-block"><?php echo _("Message or tone to be played if no trunks are available. Used on routes marked as intra-company only.")?></span>
+										</div>
+									</div>
+								</div>
+								<!--END Intra-Company Routes-->
+								<!--Emergency Routes-->
+								<div class="element-container">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="row">
+												<div class="form-group">
+													<div class="col-md-3">
+														<label class="control-label" for="emergency_msg_id"><?php echo _("Emergency Routes") ?></label>
+														<i class="fa fa-question-circle fpbx-help-icon" data-for="emergency_msg_id"></i>
+													</div>
+													<div class="col-md-9">
+														<select class="form-control" id="emergency_msg_id" name="emergency_msg_id">
+															<?php
+																echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $emergency_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
+																echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $emergency_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+																if (isset($tresults[0])) {
+																	foreach ($tresults as $tresult) {
+																		echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $emergency_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
+																	}
+																}
+															?>
+														</select>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<span id="emergency_msg_id-help" class="help-block fpbx-help-block"><?php echo _("Message or tone to be played if no trunks are available. Used on all emergency routes. Consider a message instructing callers to find an alternative means of calling emergency services such as a cell phone or alarm system panel.")?></span>
+										</div>
+									</div>
+								</div>
+								<!--END Emergency Routes-->
+							</div>
+							<div role="tabpanel" id="notrunk" class="tab-pane">
+								<!--No Answer-->
+								<div class="element-container">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="row">
+												<div class="form-group">
+													<div class="col-md-3">
+														<label class="control-label" for="no_answer_msg_id"><?php echo _("No Answer") ?></label>
+														<i class="fa fa-question-circle fpbx-help-icon" data-for="no_answer_msg_id"></i>
+													</div>
+													<div class="col-md-9">
+														<select class="form-control" id="no_answer_msg_id" name="no_answer_msg_id">
+															<?php
+																echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $no_answer_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
+																echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $no_answer_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+																if (isset($tresults[0])) {
+																	foreach ($tresults as $tresult) {
+																		echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $no_answer_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
+																	}
+																}
+															?>
+														</select>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<span id="no_answer_msg_id-help" class="help-block fpbx-help-block"><?php echo _("Message or tone to be played if there was no answer. Default message is:<br>\"The number is not answering.\"<br> Hangupcause is 18 or 19")?></span>
+										</div>
+									</div>
+								</div>
+								<!--END No Answer-->
+								<!--Number or Address Incomplete-->
+								<div class="element-container">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="row">
+												<div class="form-group">
+													<div class="col-md-3">
+														<label class="control-label" for="invalidnmbr_msg_id"><?php echo _("Number or Address Incomplete") ?></label>
+														<i class="fa fa-question-circle fpbx-help-icon" data-for="invalidnmbr_msg_id"></i>
+													</div>
+													<div class="col-md-9">
+														<select class="form-control" id="invalidnmbr_msg_id" name="invalidnmbr_msg_id">
+															<?php
+																echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $invalidnmbr_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
+																echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $invalidnmbr_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+																if (isset($tresults[0])) {
+																	foreach ($tresults as $tresult) {
+																		echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $invalidnmbr_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
+																	}
+																}
+															?>
+														</select>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<span id="invalidnmbr_msg_id-help" class="help-block fpbx-help-block"><?php echo _("Message or tone to be played if trunk reports Number or Address Incomplete. Usually this means that the number you have dialed is to short. Default message is:<br>\"The number you have dialed is not in service. Please check the number and try again.\"<br>Hangupcause is 28")?></span>
+										</div>
+									</div>
+								</div>
+								<!--END Number or Address Incomplete-->
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
