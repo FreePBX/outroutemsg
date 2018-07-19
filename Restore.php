bfe7116ca4c3361cee7d89753c02b83368e2b105
James Finstrom <jfinstrom@sangoma.com>
2018-07-19 16:43:23 +0000

    Restore cleanup

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Outroutemsg;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->getConfigs();
+    $configs = reset($this->getConfigs());
     $this->FreePBX->Outroutemsg->set($configs['default_msg_id'],  $configs['intracompany_msg_id'],  $configs['emergency_msg_id'],  $configs['no_answer_msg_id'],  $configs['invalidnmbr_msg_id']);
   }
 }
